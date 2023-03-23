c36a8cddc46a928e09f16e4dd9fe9f397fb5f0da
linaryoo <yej881@gmail.com>
2023-03-23 16:29:15 +0900

    Fix: Exceptions 수정

--- src/main/java/nextstep/helloworld/mvc/exceptions/ExceptionsController.java
+++ src/main/java/nextstep/helloworld/mvc/exceptions/ExceptionsController.java
@@ -2,11 +2,10 @@ package nextstep.helloworld.mvc.exceptions;
 
 import nextstep.helloworld.mvc.exceptions.exception.CustomException;
 import nextstep.helloworld.mvc.exceptions.exception.HelloException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/exceptions")
@@ -22,7 +21,13 @@ public class ExceptionsController {
         throw new HelloException();
     }
 
+    @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handle() {
         return ResponseEntity.badRequest().body("CustomException");
+    }
+
+    @ExceptionHandler(HelloException.class)
+    public ResponseEntity<String> handle2() {
+        return ResponseEntity.badRequest().body("HelloException");
     }
 }
